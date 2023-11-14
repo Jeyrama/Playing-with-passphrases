a0316e519545069a5409442cd4907ea2cd9378cd
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-14 13:51:50 -0800

    Update Playing-with-passphrases.js

--- Playing-with-passphrases.js
+++ Playing-with-passphrases.js
@@ -20,3 +20,20 @@ Example:
 
 
 // Solution
+
+function playPass(s, n) {
+  return s
+    .replace(/[A-Z]/g, function(char) {
+      return String.fromCharCode((char.cgarCodeAt(0) - 65 + n) % 26 + 65);
+    })
+    .replace(/[a-z]/g, function(char) {
+      return String.fromCharCode((char.charCodeAt(0) - 97 + n) % 26 + 97);
+    })
+    .replace(/\d/g, function(digit) {
+      return 9 - digit;
+    })
+    .replace(/(.)(.?)/g, function(match, odd, even) {
+      return odd.toUpperCase() + even.toLowerCase();
+    })
+    .split('').reverse().join('');
+}
